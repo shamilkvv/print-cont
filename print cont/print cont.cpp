9e638cec8c2848e86744d4e796b76584bfcb9d0e
shamilkvv <shamiltv803@gmail.com>
2023-11-21 18:26:32 +0400

    Добавьте файлы проекта.

---
+++ print cont/print cont.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <vector>
+#include <list>
+#include <deque>
+#include <array>
+
+template <typename T>
+
+void Print(const T& c, const std::string& separate) {
+	auto iter = c.begin();
+	std::cout << *iter;
+	iter++;
+	while (iter != c.end()) {
+		std::cout << separate << *iter;
+		iter++;
+	}
+}
