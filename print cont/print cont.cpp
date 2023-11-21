095bb6164aec7e927910561d6190e1c969ea03ae
shamilkvv <shamiltv803@gmail.com>
2023-11-21 18:33:42 +0400

    test Print function

--- print cont/print cont.cpp
+++ print cont/print cont.cpp
@@ -16,4 +16,18 @@ void Print(const T& c, const std::string& separate) {
 	}
 }			
 
-int main(){}
+int main()
+{
+	std::list<int> L = { 5,50,500,5000,50000 };
+	Print(L, " ");
+	std::cout << "\n";
+	std::vector<int> V = { 1,2,3,4,5 };
+	Print(V, " / ");
+	std::cout << "\n";
+	std::deque<double> Deq = { 1.5, 2.5, 3.5, 4.5 };
+	Print(Deq, " '' ");
+	std::cout << "\n";
+	std::array<int, 5> Arr = { 1, 2, 4, 8, 16 };
+	Print(Arr, " _ ");
+
+}
